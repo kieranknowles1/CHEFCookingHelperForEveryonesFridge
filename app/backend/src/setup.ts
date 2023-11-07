c341ef87d844609ecea66b797de32e1b43a94cb5
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-11-07 16:02:06 +0000

    Import CSV data into the database

--- app/backend/src/setup.ts
+++ app/backend/src/setup.ts
@@ -3,9 +3,14 @@
  * This script should be run using `npm run setup` before running the main script.
  */
 
-import { type Database } from 'sqlite'
-import path from 'path'
+import { createReadStream, statSync } from 'fs'
 import { readFile } from 'fs/promises'
+import { type Database } from 'sqlite'
+import cliProgress from 'cli-progress'
+import csv from 'csv-parse'
+import path from 'path'
+import progressTracker from 'progress-stream'
+
 import { openDb } from './openDb'
 
 // TODO: Use environment variables and put this somewhere outside the container
@@ -17,10 +22,46 @@ async function setupSchema (db: Database): Promise<void> {
   await db.exec(schema)
 }
 
+function addRow (db: Database, row: any): void {
+  // TODO: Handle the ingredients and convert directions to one string. Probably need a separate pass to gather ingredients first
+  db.run('INSERT INTO recipe (name, directions, link) VALUES (?, ?, ?)', row.title, row.directions, row.link).catch((ex) => { throw ex })
+}
+
+async function loadData (db: Database): Promise<void> {
+  // Run everything within a transaction in order to reduce I/O workload
+  await db.run('BEGIN TRANSACTION')
+  const total = statSync(INITIAL_DATA_PATH).size
+
+  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+  bar.start(total, 0)
+
+  const progressStream = progressTracker({
+    length: total,
+    time: 100 /* ms */
+  }).on('progress', (progressStat) => {
+    bar.update(progressStat.transferred)
+  })
+
+  createReadStream(INITIAL_DATA_PATH)
+    .pipe(progressStream)
+    .pipe(csv.parse({ columns: true }))
+    .on('data', (data) => {
+      addRow(db, data)
+    })
+    .on('end', () => {
+      bar.stop()
+      db.run('COMMIT').catch((ex) => { throw ex })
+    })
+}
+
 async function main (): Promise<void> {
   const db = await openDb()
 
+  console.log('Setting up schema')
   await setupSchema(db)
+
+  console.log('Importing data into the database')
+  await loadData(db)
 }
 
 main().catch((ex) => { throw ex })
