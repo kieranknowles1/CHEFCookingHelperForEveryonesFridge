f0d2bc6e8d41f68cd3cc5192d0d9c50958d27acf
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-11-07 16:51:02 +0000

    Get list of ingredient names

--- app/backend/src/setup.ts
+++ app/backend/src/setup.ts
@@ -16,9 +16,54 @@ import { openDb } from './openDb'
 const INITIAL_DATA_PATH = path.join(process.cwd(), 'working_data/full_dataset.csv')
 const SCHEMA_PATH = path.join(process.cwd(), 'data/schema.sql')
 
+/**
+ * Create a progress listener and bar and start the bar. The bar must be stopped once finished
+ * @param path
+ */
+function createTrackers(path: string): [progressTracker.ProgressStream, cliProgress.Bar] {
+  const total = statSync(path).size
+
+  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+  bar.start(total, 0)
+
+  const progress = progressTracker({
+    length: total,
+    time: 100 /* ms */
+  }).on('progress', (progressStat) => {
+    bar.update(progressStat.transferred)
+  })
+
+  return [progress, bar]
+}
+
 function setupSchema (db: Database): void {
   const schema = readFileSync(SCHEMA_PATH, 'utf-8')
   db.exec(schema)
+}
+
+async function getIngredientNames (): Promise<Set<string>> {
+  const names = new Set<string>()
+  const [progress, bar] = createTrackers(INITIAL_DATA_PATH)
+
+  return new Promise((resolve, reject) => createReadStream(INITIAL_DATA_PATH)
+    .pipe(progress)
+    .pipe(csv.parse({ columns: true }))
+    .on('data', (data) => {
+      const ingredients = JSON.parse(data.NER) as string[]
+      ingredients.forEach(item => {
+        names.add(item)
+      })
+    })
+    .on('end', () => {
+      bar.stop()
+      resolve(names)
+    }))
+}
+
+async function parseIngredients (db: Database): Promise<void> {
+  console.log('Getting ingredient names')
+  const list = await getIngredientNames()
+  console.log(list)
 }
 
 function addRow (db: Database, row: any): void {
@@ -26,23 +71,14 @@ function addRow (db: Database, row: any): void {
   db.run('INSERT INTO recipe (name, directions, link) VALUES (?, ?, ?)', row.title, row.directions, row.link)
 }
 
-function loadData (db: Database): void {
+async function importData (db: Database): Promise<void> {
   // Run everything within a transaction in order to reduce I/O workload
   db.run('BEGIN TRANSACTION')
-  const total = statSync(INITIAL_DATA_PATH).size
 
-  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
-  bar.start(total, 0)
+  const [progress, bar] = createTrackers(INITIAL_DATA_PATH)
 
-  const progressStream = progressTracker({
-    length: total,
-    time: 100 /* ms */
-  }).on('progress', (progressStat) => {
-    bar.update(progressStat.transferred)
-  })
-
-  createReadStream(INITIAL_DATA_PATH)
-    .pipe(progressStream)
+  return new Promise<void>((resolve, reject) => createReadStream(INITIAL_DATA_PATH)
+    .pipe(progress)
     .pipe(csv.parse({ columns: true }))
     .on('data', (data) => {
       addRow(db, data)
@@ -50,13 +86,23 @@ function loadData (db: Database): void {
     .on('end', () => {
       bar.stop()
       db.run('COMMIT')
+      resolve()
     })
+  )
 }
 
-const db = openDb()
+async function main (): Promise<void> {
+  const db = openDb()
 
-console.log('Setting up schema')
-setupSchema(db)
+  console.log('Setting up schema')
+  setupSchema(db)
 
-console.log('Importing data into the database')
-loadData(db)
+  console.log('Getting ingredients')
+  await parseIngredients(db)
+
+  console.log('Importing data into the database')
+  await importData(db)
+
+  console.log('Setup done')
+}
+main().catch((ex) => { throw ex })
