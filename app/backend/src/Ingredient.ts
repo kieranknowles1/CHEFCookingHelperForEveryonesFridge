ff472467670b0f8d372265bf259d9ef565a515e2
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-11-13 19:01:22 +0000

    Get unit from data

--- app/backend/src/Ingredient.ts
+++ app/backend/src/Ingredient.ts
@@ -3,14 +3,13 @@ import type ICsvRecipeRow from './ICsvRecipeRow.js'
 import Fraction from 'fraction.js'
 
 import ChefDatabase from './ChefDatabase'
-import { type Unit } from 'Unit.js'
+import { Unit } from './Unit'
 
 export type IngredientId = number
-// TODO: This should include the units and convert them to metric
 export type IngredientAmount = number
 export type IngredientMap = CiMap<IngredientId, IngredientAmount>
 
-const AMOUNT_PATTERN = /(\d+\/\d+|\d+ \d+\/\d+|\d+)/
+const AMOUNT_PATTERN = /(\d+\/\d+|\d+ \d+\/\d+|\d+) (\w+)/
 
 export class UnparsedIngredientError extends Error {}
 
@@ -20,18 +19,30 @@ export interface IIngredient {
 }
 
 export default class Ingredient implements IIngredient {
-  private static getAmount (name: string, amounts: string[]): IngredientAmount {
-    function fail (): never { throw new UnparsedIngredientError(`Could not get amount for '${name}'`) }
+  private static getAmount (ingredient: Ingredient, amounts: string[]): IngredientAmount {
+    function fail (): never { throw new UnparsedIngredientError(`Could not get amount for '${ingredient.name}'`) }
 
-    const nameLower = name.toLowerCase()
+    const nameLower = ingredient.name.toLowerCase()
     const found = amounts.find(e => e.toLowerCase().includes(nameLower))
 
     if (found === undefined) { fail() }
 
-    const match = found.match(AMOUNT_PATTERN)?.[1]
-    if (match === undefined) { fail() }
+    const match = found.match(AMOUNT_PATTERN)
+    if (match === null) { fail() }
 
-    return new Fraction(match).valueOf()
+    const amount = new Fraction(match[1]).valueOf()
+    const unit = match[2]
+
+    if (ingredient.preferredUnit === Unit.whole) {
+      // Nothing to do
+      return amount
+    } else if (ingredient.preferredUnit === Unit.none) {
+      // TODO: Return null here
+      return amount
+    } else {
+      // TODO: Convert to metric
+      return amount
+    }
   }
 
   static parseCsvRow (row: ICsvRecipeRow): IngredientMap {
@@ -46,7 +57,7 @@ export default class Ingredient implements IIngredient {
       if (ingredient === null) {
         throw new Error(`Ingredient ${name} does not exist in database`)
       }
-      map.set(ingredient.id, this.getAmount(name, amounts))
+      map.set(ingredient.id, this.getAmount(ingredient, amounts))
     }
 
     return map
