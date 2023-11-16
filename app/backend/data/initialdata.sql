056456406ee7d590fc653b078b73b271dcdf8cd9
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-11-16 16:28:58 +0000

    Update logbook

--- app/backend/data/initialdata.sql
+++ app/backend/data/initialdata.sql
@@ -18,31 +18,53 @@ INSERT INTO ingredient
     (name, preferredUnit, assumeUnlimited, density)
 VALUES
     ('Boiling Water', 'ml', true, 1.0),
+    ('Cold Water', 'ml', true, 1.0),
     ('Water', 'ml', true, 1.0);
 
 -- Source: https://annaolson.ca/baking-conversions/
+-- https://www.cookitsimply.com/measurements/cups/chicken/
+-- https://www.cookitsimply.com/measurements/cups/potatoes/
+-- https://www.cookitsimply.com/measurements/cups/rice/
+-- https://coolconversion.com/cooking-volume-weight/
 INSERT INTO ingredient
     (name, preferredUnit, density)
 VALUES
-    ('Baking Powder', 'g', 1.0),
+    ('Baking Powder', 'g', 0.9),
+    ('Baking Soda', 'g', 0.9),
     ('Brown Sugar', 'g', 0.8),
     ('Butter', 'g', 0.9),
+    ('Cheese', 'g', 0.4),
+    ('Chicken', 'g', 0.5),
     ('Flour', 'g', 0.6),
     ('Ground Beef', 'g', 1.0),
     ('Milk', 'ml', 1.0),
+    ('Mushrooms', 'g', 0.4),
     ('Nuts', 'g', 0.5),
     ('Oil', 'ml', 0.9),
+    ('Potatoes', 'g', 0.9),
+    ('Rice', 'g', 0.9),
     ('Sugar', 'g', 0.8),
-    ('Vanilla', 'ml', 0.9);
+    ('Vanilla', 'ml', 0.9),
+    ('Vinegar', 'ml', 0.9);
 
 INSERT INTO ingredient_alt_name
     (name, ingredient_id)
 VALUES
+    ('Black Pepper', (SELECT id FROM ingredient WHERE name = 'Pepper')),
+    ('Carrot', (SELECT id FROM ingredient WHERE name = 'Carrots')),
+    ('Cheddar Cheese', (SELECT id FROM ingredient WHERE name = 'Cheese')),
+    ('Clove Garlic', (SELECT id FROM ingredient WHERE name = 'Garlic')),
+    ('Egg Yolks', (SELECT id FROM ingredient WHERE name = 'Eggs')),
     ('Egg', (SELECT id FROM ingredient WHERE name = 'Eggs')),
+    ('Freshly Ground Black Pepper', (SELECT id FROM ingredient WHERE name = 'Pepper')),
     ('Green Pepper', (SELECT id FROM ingredient WHERE name = 'Bell Pepper')),
     ('Margarine', (SELECT id FROM ingredient WHERE name = 'Butter')),
+    ('Mozzarella Cheese', (SELECT id FROM ingredient WHERE name = 'Cheese')),
     ('Olive Oil', (SELECT id FROM ingredient WHERE name = 'Oil')),
     ('Onion', (SELECT id FROM ingredient WHERE name = 'Onions')),
     ('Red Bell Pepper', (SELECT id FROM ingredient WHERE name = 'Bell Pepper')),
     ('Red Pepper', (SELECT id FROM ingredient WHERE name = 'Bell Pepper')),
+    ('Red Wine Vinegar', (SELECT id FROM ingredient WHERE name = 'Vinegar')),
+    ('Tomato', (SELECT id FROM ingredient WHERE name = 'Tomatoes')),
+    ('Unsalted Butter', (SELECT id FROM ingredient WHERE name = 'Butter')),
     ('White Sugar', (SELECT id FROM ingredient WHERE name = 'Sugar'));
--- app/backend/src/setup.ts
+++ app/backend/src/setup.ts
@@ -104,6 +104,6 @@ async function main (): Promise<void> {
   logger.log('info', `Setup done, imported ${dataInfo.success} of ${dataInfo.total} recipes (${(dataInfo.success / dataInfo.total) * 100}%)`)
 
   const missedFrequencies = Array.from(missedIngredients).sort((a, b) => b[1] - a[1])
-  console.log(missedFrequencies)
+  logger.log('info', `Missing ingredients by frequency: ${missedFrequencies.toString()}`)
 }
 main().catch((err) => { logError(err) })
