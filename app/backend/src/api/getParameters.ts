ed02805c5f18a04a5884715ba4a770d274953f80
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-12-05 14:25:23 +0000

    Add note about wrong code

--- app/backend/src/api/getParameters.ts
+++ app/backend/src/api/getParameters.ts
@@ -3,11 +3,16 @@ import { matchedData } from 'express-validator'
 
 import { type EndpointParameters, type JsonEndpointWithParameters } from './TypedEndpoint'
 
+type MatchedData<endpoint extends JsonEndpointWithParameters> = Record<keyof EndpointParameters<endpoint>, string>
+
 /**
  * Function to get strongly typed parameters from the matched data that was validated using `express-validator`.
  *
  * Use `checkParameters` as middleware to validate that the data exists in the expected format.
+ *
+ * NOTE: Parameters will be strings, even if matched against other types
+ * // FIXME: Returning `any` as matchedData doesn't work how I thought it did. It keeps matched numbers as strings
  */
-export default function getParameters<endpoint extends JsonEndpointWithParameters> (req: Request): EndpointParameters<endpoint> {
-  return matchedData(req) as EndpointParameters<endpoint>
+export default function getParameters<endpoint extends JsonEndpointWithParameters> (req: Request): any {
+  return matchedData(req) as MatchedData<endpoint>
 }
