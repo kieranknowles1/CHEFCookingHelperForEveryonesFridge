3315d8a93ac1f91a6d5bc959f5d4c1c1fe6dd98f
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-12-21 23:24:20 +0000

    Remove package with security vulnerabilities

--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+const ICON_OPTIONS = ['👩‍🍳', '👩🏻‍🍳', '👩🏼‍🍳', '👩🏽‍🍳', '👩🏾‍🍳', '👩🏿‍🍳', '👨‍🍳', '👨🏻‍🍳', '👨🏼‍🍳', '👨🏽‍🍳', '👨🏾‍🍳', '👨🏿‍🍳']
+
 export default function HomePage (): React.JSX.Element {
+  const [icon] = React.useState(ICON_OPTIONS[Math.floor(Math.random() * ICON_OPTIONS.length)])
+  console.log(icon)
+
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1>{icon} - Cooking Helper for Everyone&apos;s Fridge</h1>
     </main>
   )
 }
