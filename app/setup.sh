a7ada43da2f8498bbef50326f09dcf1cf84b399a
kieranknowles1 <kieranknowles1@users.noreply.github.com>
2023-12-30 17:18:55 +0000

    [MegaLinter] Apply linters fixes

--- app/setup.sh
+++ app/setup.sh
@@ -4,9 +4,9 @@ DEPS="npm"
 
 # Require root
 # https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run as root"
+	exit
 fi
 
 # Install dependencies
@@ -17,25 +17,25 @@ apt-get install $DEPS
 
 # Ask to download dataset if not found
 if [ ! -f "./backend/working_data/full_dataset.csv" ]; then
-    echo "Dataset not found. Please download it from https://recipenlg.cs.put.poznan.pl/ and place it in ./backend/working_data/full_dataset.csv"
-    exit
+	echo "Dataset not found. Please download it from https://recipenlg.cs.put.poznan.pl/ and place it in ./backend/working_data/full_dataset.csv"
+	exit
 fi
 
 echo "Setting up backend..."
 (
-  cd backend || exit
-  npm install --save-dev
-  npm run build
-  npm run setup
+	cd backend || exit
+	npm install --save-dev
+	npm run build
+	npm run setup
 )
 
 cd ..
 
 echo "Setting up frontend..."
 (
-  cd frontend || exit
-  npm install --save-dev
-  npm run build
+	cd frontend || exit
+	npm install --save-dev
+	npm run build
 )
 
 # Run app
