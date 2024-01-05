95ddb3ee6f28a95826720edd372183617b0dff68
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-01-05 00:58:30 +0000

    Use a single docker compose for everything

--- app/setup.sh
+++ app/setup.sh
@@ -3,6 +3,7 @@
 # Exit if any command fails (returns non-zero exit code)
 set -e
 
+DATASET_PATH="./dataset/full_dataset.csv"
 DEPS="docker docker-compose"
 
 echo "Checking and installing dependencies $DEPS"
@@ -10,32 +11,17 @@ echo "This may ask for your password"
 sudo apt-get update
 sudo apt-get install $DEPS
 
-# Setup app
-
-# Ask to download dataset if not found
-if [ ! -f "./backend/working_data/full_dataset.csv" ]; then
-	echo "Dataset not found. Please download it from https://recipenlg.cs.put.poznan.pl/ and place it in ./backend/working_data/full_dataset.csv"
-	exit
+# Check if dataset exists
+if [ ! -f "$DATASET_PATH" ]; then
+	echo "Dataset not found. Please download it from https://recipenlg.cs.put.poznan.pl/ and place it in $DATASET_PATH"
+	exit 1
 fi
 
-echo "Building backend container. Hope you have a SSD, because Node creates a ridiculous amount of files"
-(
-	cd backend
-	docker-compose build
-)
+echo "Running setup script"
+docker-compose --env-file backend.env run --rm backend npm run setup
 
-# Run app
+echo "Building containers"
+docker-compose build
 
-echo "Setting up backend. Grab a cuppa, this will take a while"
-(
-	cd backend
-	docker-compose run --rm chef npm run setup
-)
-
-# TODO: Build and run the frontend
-
-echo "Starting backend"
-(
-	cd backend
-	docker-compose up -d
-)
+echo "Starting containers"
+docker-compose up -d
