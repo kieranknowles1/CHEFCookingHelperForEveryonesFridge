76b38bbafa754d8ad4b2674afef12994c3efccc8
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-01-20 22:32:26 +0000

    Move ingredient picker to its own component

--- app/frontend/src/pages/myfridge/AddIngredient.tsx
+++ app/frontend/src/pages/myfridge/AddIngredient.tsx
@@ -1,11 +1,9 @@
-import { Combobox } from '@headlessui/react'
 import React from 'react'
 
-import LoadingSpinner, { type LoadingStatus } from '../../components/LoadingSpinner'
+import { IngredientPicker } from '../../components/IngredientPicker'
 import UserContext from '../../contexts/UserContext'
 import apiClient from '../../apiClient'
 import { type components } from '../../types/api.generated'
-import monitorStatus from '../../utils/monitorStatus'
 import useSafeContext from '../../contexts/useSafeContext'
 
 type Ingredient = components['schemas']['Ingredient']
@@ -20,35 +18,8 @@ export default function AddIngredient (props: AddIngredientProps): React.JSX.Ele
 
   const context = useSafeContext(UserContext)
 
-  const [status, setStatus] = React.useState<LoadingStatus>('loading')
-  const [ingredients, setIngredients] = React.useState<Ingredient[]>([])
-
   const [selected, setSelected] = React.useState<Ingredient | null>(null)
   const [amount, setAmount] = React.useState(0)
-
-  const [filteredIngredients, setFilteredIngredients] = React.useState<Ingredient[]>([])
-  const [query, setQuery] = React.useState('')
-
-  React.useEffect(() => {
-    apiClient.GET(
-      '/ingredient/all',
-      { }
-    ).then(
-      monitorStatus(setStatus)
-    ).then(data => {
-      setIngredients(data.filter(
-        ingredient => !ingredient.assumeUnlimited && !currentIngredientIds.has(ingredient.id)
-      ))
-    }).catch(err => {
-      console.error(err)
-    })
-  }, [])
-
-  React.useEffect(() => {
-    setFilteredIngredients(ingredients.filter(
-      ingredient => ingredient.name.toLowerCase().includes(query.toLowerCase())
-    ))
-  }, [ingredients, query])
 
   function onSubmit (event: React.FormEvent): void {
     event.preventDefault()
@@ -74,44 +45,22 @@ export default function AddIngredient (props: AddIngredientProps): React.JSX.Ele
   }
 
   return (
-    <>
-      <LoadingSpinner status={status} />
-      {status === 'done' && (
-        <form onSubmit={onSubmit}>
-          <label>Ingredient:
-            <Combobox value={selected} onChange={setSelected}>
-              <Combobox.Input
-                className='bg-raisin_black-800 text-citron-100'
-                onChange={(event) => { setQuery(event.target.value) }}
-                displayValue={(ingredient: Ingredient | null) => ingredient?.name ?? ''}
-                autoFocus
-                required
-              />
-              <Combobox.Options>
-                {filteredIngredients.map(ingredient => (
-                  <Combobox.Option key={ingredient.id} value={ingredient}>
-                    {({ active }) => (
-                      <li className={active ? 'bg-tiffany_blue-200' : 'bg-tiffany_blue-100'}>
-                        {ingredient.name}
-                      </li>
-                    )}
-                  </Combobox.Option>
-                ))}
-              </Combobox.Options>
-            </Combobox>
-          </label><br />
+    <form onSubmit={onSubmit}>
+      <label>Ingredient: <IngredientPicker
+        excludedIds={currentIngredientIds}
+        selected={selected}
+        setSelected={setSelected}
+      /></label><br />
 
-          <label>Amount: <input
-            type='number'
-            min={0}
-            onChange={event => { setAmount(Number.parseFloat(event.target.value)) }}
-            className='w-1/2'
-            required
-          /></label><br />
+      <label>Amount: <input
+        type='number'
+        min={0}
+        onChange={event => { setAmount(Number.parseFloat(event.target.value)) }}
+        className='w-1/2'
+        required
+      /></label><br />
 
-          <button type='submit' className='bg-savoy_blue-400 rounded'>Submit</button>
-        </form>
-      )}
-    </>
+      <button type='submit' className='bg-savoy_blue-400 rounded'>Submit</button>
+    </form>
   )
 }
