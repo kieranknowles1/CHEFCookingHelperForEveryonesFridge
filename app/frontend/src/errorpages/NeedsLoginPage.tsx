d4baf833a5ea5c9957e7bae57175d85633bcb32d
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-01-23 21:08:19 +0000

    Add page for when not logged in

---
+++ app/frontend/src/errorpages/NeedsLoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * Page to display when a user needs to login to view a page.
+ */
+export default function NeedsLoginPage (): React.JSX.Element {
+  return (
+    <div>
+      <h1>Login Required</h1>
+      <p>Please login to view this page.</p>
+    </div>
+  )
+}
