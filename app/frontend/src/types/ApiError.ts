73a6db21b60edc0e3b6770972813107c4699e230
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-01-27 17:06:53 +0000

    Add code to API error

--- app/frontend/src/types/ApiError.ts
+++ app/frontend/src/types/ApiError.ts
@@ -4,10 +4,12 @@ type ErrorList = components['schemas']['ErrorList']
 
 export class ApiError extends Error {
   public readonly errors: ErrorList
+  public readonly code: number
 
   constructor (errors: ErrorList) {
     super(errors.errors[0].message)
     this.name = ApiError.name
     this.errors = errors
+    this.code = errors.status
   }
 }
