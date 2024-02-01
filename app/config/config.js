48d7597313d7ed2b82faabfd6224107b2bb144c4
Kieran <kieranknowles11@hotmail.co.uk>
2024-02-01 11:01:08 +0000

    Point API to my domain

--- app/config/config.js
+++ app/config/config.js
@@ -6,5 +6,5 @@
 // NOTE: This must be declared as a var, not a const, so it will be available in the window object.
 /** @type {import('../frontend/src/config').Config} */
 var config = {
-  API_BASE_URL: 'http://localhost:3000/api/v1'
+  API_BASE_URL: 'https://chefapi.knowles.freemyip.com/api/v1'
 }
