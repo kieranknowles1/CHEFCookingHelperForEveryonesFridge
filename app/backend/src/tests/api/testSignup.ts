088720407d2e92a47a38765cbde712c00645c509
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-28 15:13:11 +0000

    Remove more duplicate code

--- app/backend/src/tests/api/testSignup.ts
+++ app/backend/src/tests/api/testSignup.ts
@@ -7,12 +7,20 @@ import request from 'supertest'
 
 import createTestApp, { type TestApp } from './createTestApp'
 
+function makeRequest (app: TestApp, username: string, password: string): request.Test {
+  return request(app.server)
+    .post('/api/v1/signup')
+    .set('Authorization', createHeader(username, password))
+}
+
 function createHeader (username: string, password: string): string {
   return `Basic ${btoa(`${username}:${password}`)}`
 }
 
 describe('/api/v1/signup', () => {
   let app: TestApp
+  const username = faker.internet.userName()
+  const password = faker.internet.password()
   beforeEach(() => {
     app = createTestApp()
   })
@@ -22,12 +30,7 @@ describe('/api/v1/signup', () => {
   })
 
   it('should sign up a new user', async () => {
-    const username = faker.internet.userName()
-    const password = faker.internet.password()
-
-    await request(app.server)
-      .post('/api/v1/signup')
-      .set('Authorization', createHeader(username, password))
+    await makeRequest(app, username, password)
       .expect(201)
 
     await request(app.server)
@@ -37,12 +40,7 @@ describe('/api/v1/signup', () => {
   })
 
   it('should return a valid token for the new user', async () => {
-    const username = faker.internet.userName()
-    const password = faker.internet.password()
-
-    const response = await request(app.server)
-      .post('/api/v1/signup')
-      .set('Authorization', createHeader(username, password))
+    const response = await makeRequest(app, username, password)
       .expect(201)
 
     const { token, userId } = response.body
@@ -62,22 +60,13 @@ describe('/api/v1/signup', () => {
   })
 
   it('should not allow signing up with an existing username, case insensitive', async () => {
-    const username = faker.internet.userName()
-    const password = faker.internet.password()
-
-    await request(app.server)
-      .post('/api/v1/signup')
-      .set('Authorization', createHeader(username, password))
+    await makeRequest(app, username, password)
       .expect(201)
 
-    await request(app.server)
-      .post('/api/v1/signup')
-      .set('Authorization', createHeader(username.toLowerCase(), password))
+    await makeRequest(app, username.toLowerCase(), password)
       .expect(400)
 
-    await request(app.server)
-      .post('/api/v1/signup')
-      .set('Authorization', createHeader(username.toUpperCase(), password))
+    await makeRequest(app, username.toUpperCase(), password)
       .expect(400)
   })
 })
