d3bafa5acadcafca9bf9470d603e32a587598666
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-03-07 12:54:36 +0000

    Some research work

--- app/backend/src/ml/getSimilarity.ts
+++ app/backend/src/ml/getSimilarity.ts
@@ -1,14 +1,12 @@
 import * as tf from '@tensorflow/tfjs-core'
 
 /**
- * Get the similarity between two embeddings.
+ * Get the cosine similarity between two embeddings.
  *
  * @returns A number between 0 and 1, where 1 is the most similar.
  */
 export default function getSimilarity (embedding1: Float32Array, embedding2: Float32Array): number {
-  // TODO: Which metric? Need a source in the paper.
-  // Using cosine similarity for now.
-  // https://en.wikipedia.org/wiki/Cosine_similarity
+  // Use cosine similarity, as done in the original USE paper.
   const dotProduct = tf.dot(tf.tensor(embedding1), tf.tensor(embedding2))
   const norm1 = tf.norm(tf.tensor(embedding1))
   const norm2 = tf.norm(tf.tensor(embedding2))
